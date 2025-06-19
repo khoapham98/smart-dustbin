ebbe6890e0260289eec5f580b24c7b6712776fd0
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-19 19:05:48 +0700

    test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,9 +2,11 @@
 #include "mydelay.h"
 #include "hcsr04.h"
 #include "uart.h"
+#include "servo.h"
 
 uint32_t distance = 0;
 
+void I2C_Init();
 
 int main()
 {
@@ -26,5 +28,28 @@ int main()
 
 	}
 	return 0;
+}
+
+/*
+ * This function is used to initialize I2C1 peripheral
+ * PB6: SCL
+ * PB7: SDA
+ */
+void I2C_Init()
+{
+	uint32_t* GPIOB_MODER = (uint32_t*) (GPIOB_BASE_ADDR + 0x00);
+	uint32_t* GPIOB_OTYPER = (uint32_t*) (GPIOB_BASE_ADDR + 0x04);
+	uint32_t* GPIOB_PUPDR = (uint32_t*) (GPIOB_BASE_ADDR + 0x0C);
+	uint32_t* GPIOB_AFRL = (uint32_t*) (GPIOB_BASE_ADDR + 0x20);
+	*GPIOB_MODER &= ~(0xf << 12);	// clear bit
+	*GPIOB_MODER |= (0b1010 << 12);	// set PB6, PB7 at AF mode
+	*GPIOB_OTYPER |= (0b11 << 6);	// set open-drain mode
+	*GPIOB_PUPDR &= ~(0xf << 12);	// clear bit
+	*GPIOB_PUPDR |= (0b0101 << 12);	// configure PB6, PB7 the I/O pull-up
+	*GPIOB_AFRL &= ~(0xff << 24);	// clear bit
+	*GPIOB_AFRL |= (4 << 24) | (4 << 28);	// select AF04
+
+	__HAL_RCC_I2C1_CLK_ENABLE();
+	*GPIOB_AFRL |= (4 << 24) | (4 << 28);	// select AF04
 }
 
